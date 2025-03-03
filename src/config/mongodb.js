78fd578dff04c3a78fa6cdc8102f47ead573d0d7
unknown <anhkho881@gmail.com>
2025-03-03 22:55:38 +0700

    async code with remote

---
+++ src/config/mongodb.js
@@ -0,0 +1,5 @@
+//JM4N18jLqQQ0V3jm
+//anhkho881
+
+const MONGODB_URI = 'mongodb+srv://anhkho881:JM4N18jLqQQ0V3jm@cluster0.3jfts.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DB_NAME = 'LacVietStudio'
