6a4c24c32af9423ca7fe42d1b4dce3a2cd588f72
dungvudinh <anhkho881@gmail.com>
2025-05-20 16:22:12 +0700

    remove sameSite = strict

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -100,7 +100,7 @@ const login = async (req, res, next) =>
     res.cookie("refreshToken", refreshToken, {
       httpOnly:true, 
       secure:true, 
-      sameSite:'Strict', 
+      sameSite:'None', 
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 ngày
     })
     res.status(StatusCodes.OK).json({
